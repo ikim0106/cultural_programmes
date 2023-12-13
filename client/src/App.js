0406421980172acb0151e0baf93c5bc712e225f6
MakWingChit <makwingchit@gmail.com>
2023-12-13 23:37:33 +0800

    no more location page

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import UserPage from './Pages/UserPage';
 import RegisterLogin from './Pages/RegisterLogin';
 import AdminPage from './Pages/AdminPage';
 
-import LocationPage from './Pages/LocationPage';
 import LocationDetailPage from './Pages/LocationDetailPage';
 import UserFavouriteLocation from './Pages/UserFavouriteLocationPage';
 function App() {
@@ -13,7 +12,6 @@ function App() {
     <Routes>
       <Route path="/" element={<RegisterLogin />} />
       <Route path="/user" element={<UserPage />} />
-      <Route path="/LocationPage" element={<LocationPage />} />
       <Route path="/LocationDetailPage" element={<LocationDetailPage />} />
       <Route path="/admin" element={<AdminPage />} />
       <Route path="/myFavourite" element={<UserFavouriteLocation />} />
