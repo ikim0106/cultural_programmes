f0119766d75e2b618a77b6fd728c7c3a88e2c851
DireatEX <ngtikwai2017@gmail.com>
2023-12-15 03:50:59 +0800

    finish the filter for price of events

--- client/src/Components/Table.js
+++ client/src/Components/Table.js
@@ -6,6 +6,15 @@ import {
   MaterialReactTable,
   useMaterialReactTable,
 } from "material-react-table";
+import e from "cors";
+
+function calculatePrice(price) {
+  if (price.includes("Free")) { price = 0; }
+  else if (price.includes("HK$")) { price = parseInt(price.slice(3)); }
+  else if (price.includes("$")) { price = parseInt(price.slice(1)); }
+  else { price = 0; }
+  return price;
+}
 
 function Table(mode) {
   console.log(mode.mode, mode.id);
@@ -13,8 +22,95 @@ function Table(mode) {
   const [venues, SetVenues] = useState([]);
   const [userData, setUserData] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [max, setMax] = useState(0);
   const nagivate = useNavigate();
   let columns = [];
+
+  const viewLocationDetails = (venue) => {
+    console.log("venue", venue);
+    venue = venues.filter((val, key) => {
+      return val.venuee === venue ? val : null;
+    });
+    console.log("new venue", venue);
+    nagivate("/LocationDetailPage", { state: venue[0] });
+  };
+
+  useEffect(() => {
+    const getData = () => {
+      if (mode.mode === "allevent") {
+        const getAllEvent = async () => {
+          let response = await fetch("http://localhost:8080/getAllEvent", {
+            method: "Get",
+            headers: {
+              Authorization: userData?.user?.userId,
+            },
+          });
+          let data = await response.json();
+          if (data.success) console.log(data.events);
+          setData(data.events);
+          setMax(Math.max(...data.events.map((event) => calculatePrice(event.pricee))));
+          console.log("max", max);
+        };
+        getAllEvent();
+      } else if (mode.mode === "venue") {
+        const getAllVenue = async () => {
+          let response = await fetch("http://localhost:8080/getAllVenue", {
+            method: "Get",
+            headers: {
+              Authorization: userData?.user?.userId,
+            },
+          });
+          let data = await response.json();
+          console.log(data);
+          if (data.success) console.log(data.venues);
+          SetVenues(data.venues);
+          setData(
+            data.venues.map((venue) => {
+              return {
+                venuee: venue.venuee,
+                events: venue.events.length,
+              };
+            })
+          );
+        };
+        getAllVenue();
+      } else if (mode.mode === "event") {
+        const getEvent = async () => {
+          let response = await fetch(
+            `http://localhost:8080/getEvent/${mode.id}`,
+            {
+              method: "Get",
+              headers: {
+                Authorization: userData?.user?.userId,
+              },
+            }
+          );
+          let data = await response.json();
+          console.log(data)
+          if (data.success) console.log(data.events);
+          setData(data.events);
+          setMax(Math.max(...data.events.map((event) => calculatePrice(event.pricee))));
+          console.log("max", max);
+        };
+        getEvent();
+      }
+    };
+
+    const tempJSON = JSON.parse(localStorage.getItem("userData"));
+    console.log("tempJSON", tempJSON);
+    if (localStorage.getItem("userData") && tempJSON.user.role === "user") {
+      setUserData(JSON.parse(localStorage.getItem("userData")));
+      if (userData?.user?.userId) {
+        getData();
+        setIsLoading(false);
+      }
+    } else {
+      window.location.href = "/";
+    }
+  }, [mode.mode, mode.id, userData?.user?.userId]);
+
+
+
   if (mode.mode === "allevent" || mode.mode === "event") {
     columns = [
       {
@@ -52,7 +148,26 @@ function Table(mode) {
         header: "Price",
         enableEditing: true,
         size: 80,
-      },
+        filterVariant: 'range-slider',
+        muiFilterSliderProps: {
+          max: max, //custom max (as opposed to faceted max)
+          min: 0, //custom min (as opposed to faceted min)
+          valueLabelFormat: (value) =>
+            value.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            }),
+        },
+        filterFn: (row, id, filterValue) => {
+          console.log("filter:", filterValue);
+          let value = row.getValue(id);
+          if (value.includes("Free")) { value = 0; }
+          else if (value.includes("HK$")) { value = parseInt(value.slice(3)); }
+          else if (value.includes("$")) { value = parseInt(value.slice(1)); }
+          console.log("filter:", row.getValue(id), filterValue, value);
+          return value <= filterValue[1] && value >= filterValue[0];
+        },
+      }
     ];
   } else if (mode.mode === "venue") {
     columns = [
@@ -94,84 +209,6 @@ function Table(mode) {
     editDisplayMode: "modal",
   });
 
-  const viewLocationDetails = (venue) => {
-    console.log("venue", venue);
-    venue = venues.filter((val, key) => {
-      return val.venuee === venue ? val : null;
-    });
-    console.log("new venue", venue);
-    nagivate("/LocationDetailPage", { state: venue[0] });
-  };
-
-  useEffect(() => {
-    const getData = () => {
-      if (mode.mode === "allevent") {
-        const getAllEvent = async () => {
-          let response = await fetch("http://localhost:8080/getAllEvent", {
-            method: "Get",
-            headers: {
-              Authorization: userData?.user?.userId,
-            },
-          });
-          let data = await response.json();
-          if (data.success) console.log(data.events);
-          setData(data.events);
-        };
-        getAllEvent();
-      } else if (mode.mode === "venue") {
-        const getAllVenue = async () => {
-          let response = await fetch("http://localhost:8080/getAllVenue", {
-            method: "Get",
-            headers: {
-              Authorization: userData?.user?.userId,
-            },
-          });
-          let data = await response.json();
-          console.log(data);
-          if (data.success) console.log(data.venues);
-          SetVenues(data.venues);
-          setData(
-            data.venues.map((venue) => {
-              return {
-                venuee: venue.venuee,
-                events: venue.events.length,
-              };
-            })
-          );
-        };
-        getAllVenue();
-      } else if (mode.mode === "event") {
-        const getEvent = async () => {
-          let response = await fetch(
-            `http://localhost:8080/getEvent/${mode.id}`,
-            {
-              method: "Get",
-              headers: {
-                Authorization: userData?.user?.userId,
-              },
-            }
-          );
-          let data = await response.json();
-          console.log(data)
-          if (data.success) console.log(data.events);
-          setData(data.events);
-        };
-        getEvent();
-      }
-    };
-
-    const tempJSON = JSON.parse(localStorage.getItem("userData"));
-    console.log("tempJSON", tempJSON);
-    if (localStorage.getItem("userData") && tempJSON.user.role === "user") {
-      setUserData(JSON.parse(localStorage.getItem("userData")));
-      if (userData?.user?.userId) {
-        getData();
-        setIsLoading(false);
-      }
-    } else {
-      window.location.href = "/";
-    }
-  }, [mode.mode, mode.id, userData?.user?.userId]);
 
   return (
     <>
