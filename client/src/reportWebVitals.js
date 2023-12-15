faf50b4b3f2e4b887f5a0f2162b65f7d130b74ca
DireatEX <ngtikwai2017@gmail.com>
2023-12-15 23:12:39 +0800

    also add comment to some on9 pages

--- client/src/reportWebVitals.js
+++ client/src/reportWebVitals.js
@@ -1,3 +1,5 @@
+// Student Name : Ng Tik Wai, Inho Kim, Chan Yau Ki, Mak Wing Chit, Ngai Wai Ki
+// Student ID : 1155151991, 1155116159, 1155157432, 1155157789, 1155158093
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,5 +1,7 @@
+// Student Name : Ng Tik Wai, Inho Kim, Chan Yau Ki, Mak Wing Chit, Ngai Wai Ki
+// Student ID : 1155151991, 1155116159, 1155157432, 1155157789, 1155158093
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
